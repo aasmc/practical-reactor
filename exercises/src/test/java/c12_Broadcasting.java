07aacf90764bb626491f3888c03bb53e5cd3b734
aleksas <email@email.com>
2025-04-19 15:26:21 +0300

    solve broadcasting

--- exercises/src/test/java/c12_Broadcasting.java
+++ exercises/src/test/java/c12_Broadcasting.java
@@ -8,15 +8,15 @@ import java.util.concurrent.CopyOnWriteArrayList;
 /**
  * In this chapter we will learn difference between hot and cold publishers,
  * how to split a publisher into multiple and how to keep history so late subscriber don't miss any updates.
- *
+ * <p>
  * Read first:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#reactor.hotCold
  * https://projectreactor.io/docs/core/release/reference/#which.multicasting
  * https://projectreactor.io/docs/core/release/reference/#advanced-broadcast-multiple-subscribers-connectableflux
- *
+ * <p>
  * Useful documentation:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#which-operator
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html
@@ -32,8 +32,8 @@ public class c12_Broadcasting extends BroadcastingBase {
     @Test
     public void sharing_is_caring() throws InterruptedException {
         Flux<Message> messages = messageStream()
-                //todo: do your changes here
-                ;
+                .publish()
+                .refCount(2);
 
         //don't change code below
         Flux<String> userStream = messages.map(m -> m.user);
@@ -49,7 +49,7 @@ public class c12_Broadcasting extends BroadcastingBase {
 
         Assertions.assertEquals(Arrays.asList("user#0", "user#1", "user#2", "user#3", "user#4"), metaData);
         Assertions.assertEquals(Arrays.asList("payload#0", "payload#1", "payload#2", "payload#3", "payload#4"),
-                                payload);
+                payload);
     }
 
     /**
@@ -60,18 +60,18 @@ public class c12_Broadcasting extends BroadcastingBase {
     @Test
     public void hot_vs_cold() {
         Flux<String> updates = systemUpdates()
-                //todo: do your changes here
-                ;
+                .publish()
+                .autoConnect();
 
         //subscriber 1
         StepVerifier.create(updates.take(3).doOnNext(n -> System.out.println("subscriber 1 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
+                .verifyComplete();
 
         //subscriber 2
         StepVerifier.create(updates.take(4).doOnNext(n -> System.out.println("subscriber 2 got: " + n)))
-                    .expectNext("DISK_SPACE_LOW", "OOM_DETECTED", "CRASHED", "UNKNOWN")
-                    .verifyComplete();
+                .expectNext("DISK_SPACE_LOW", "OOM_DETECTED", "CRASHED", "UNKNOWN")
+                .verifyComplete();
     }
 
     /**
@@ -82,18 +82,17 @@ public class c12_Broadcasting extends BroadcastingBase {
     @Test
     public void history_lesson() {
         Flux<String> updates = systemUpdates()
-                //todo: do your changes here
-                ;
+                .cache();
 
         //subscriber 1
         StepVerifier.create(updates.take(3).doOnNext(n -> System.out.println("subscriber 1 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
+                .verifyComplete();
 
         //subscriber 2
         StepVerifier.create(updates.take(5).doOnNext(n -> System.out.println("subscriber 2 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY", "DISK_SPACE_LOW", "OOM_DETECTED")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY", "DISK_SPACE_LOW", "OOM_DETECTED")
+                .verifyComplete();
     }
 
 }
